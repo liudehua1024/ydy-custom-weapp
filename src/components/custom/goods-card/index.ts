80134651aed921604d1e27d135f4fd4caf0a2bb7
1 <>
2021-09-15 16:16:45 +0800

    调整加入和移除购物车动画逻辑

--- src/components/custom/goods-card/index.ts
+++ src/components/custom/goods-card/index.ts
@@ -12,18 +12,7 @@ Component({
 		}
 	},
 	data: {
-		maxCount: 0,
-		_goodsLogoRect: { top: 0, left: 0, width: 0, height: 0 }
-	},
-	lifetimes: {
-		ready() {
-			this.createSelectorQuery()
-				.select('#goods-logo-block')
-				.boundingClientRect((result) => {
-					if (result) this.setData({ _goodsLogoRect: result });
-				})
-				.exec();
-		}
+		maxCount: 0
 	},
 	methods: {
 		obGoodsInfo(goodsInfo: GoodsInfo) {
@@ -36,46 +25,53 @@ Component({
 			this.setData({ maxCount });
 		},
 		startAnim(opt: GoodsCarAnimOption, callback?: () => void) {
-			const { top, left, width, height } = this.data._goodsLogoRect;
-			const r1 = wx.$numUtils.toInt((opt.top - top) / 100);
-			const r2 = wx.$numUtils.toInt((opt.left - left) / 100);
-			let time = 100;
-			if (r1 > 0 || r2 > 0) {
-				if (r1 >= r2) time = r1 * 100;
-				else time = r2 * 100;
-			}
+			this.createSelectorQuery()
+					.select('#goods-logo-block')
+					.boundingClientRect((result) => {
+						if (!result) return;
+						const { top, left, width, height } = result;
+						const r1 = wx.$numUtils.toInt((opt.top - top) / 100);
+						const r2 = wx.$numUtils.toInt((opt.left - left) / 100);
+						let time = 100;
+						if (r1 > 0 || r2 > 0) {
+							if (r1 >= r2) time = r1 * 100;
+							else time = r2 * 100;
+						}
+						if (time > 800) time = 800;
 
-			let keyFrames = [
-				{
-					opacity: opt.opacity === 1 ? 0 : 1,
-					top: top + 'px',
-					left: left + 'px',
-					width: width + 'px',
-					height: height + 'px'
-				},
-				{
-					opacity: opt.opacity,
-					top: opt.top + 'px',
-					left: opt.left + 'px',
-					width: opt.width + 'px',
-					height: opt.height + 'px'
-				}
-			];
+						let keyFrames = [
+							{
+								opacity: opt.opacity === 1 ? 0 : 1,
+								top: (top + opt.scrollTop) + 'px',
+								left: left + 'px',
+								width: width + 'px',
+								height: height + 'px'
+							},
+							{
+								opacity: opt.opacity,
+								top: (opt.top + opt.scrollTop) + 'px',
+								left: opt.left + 'px',
+								width: opt.width + 'px',
+								height: opt.height + 'px'
+							}
+						];
 
-			if (opt.opacity === 1) {
-				keyFrames = keyFrames.reverse();
-			}
+						if (opt.opacity === 1) {
+							keyFrames = keyFrames.reverse();
+						}
 
-			this.animate(
-				'#anim-goods-logo',
-				keyFrames,
-				time,
-				function () {
-					if (wx.$isUtils.isFun(callback)) {
-						callback();
-					}
-				}.bind(this)
-			);
+						this.animate(
+							'#anim-goods-logo',
+							keyFrames,
+							time,
+							function() {
+								if (wx.$isUtils.isFun(callback)) {
+									callback();
+								}
+							}.bind(this)
+						);
+					})
+					.exec();
 		},
 		ignoreClick() {
 			//拦截counter的点击事件,防止误触
